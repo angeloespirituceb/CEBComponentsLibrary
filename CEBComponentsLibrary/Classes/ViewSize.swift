328eb04b8970ae6452adf6133f487c6b86bfbbe0
Michael Raffin <michaelraffinpaculba@gmail.com>
2019-12-27 13:11:25 +0800

    Revert files and removing Utils

--- CEBComponentsLibrary/Classes/ViewSize.swift
+++ CEBComponentsLibrary/Classes/ViewSize.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 
-class ViewSizeUtil {
-    
-    //should make adjustments to headerHeight when UIDevice has top notch
-    static func getHeaderHeight() -> CGFloat {
-        if UIDevice.current.hasNotch {
-            return 104.98
-        } else {
-            return 80.98
-        }
-    }
-}
+//class ViewSizeUtil {
+//    
+//    //should make adjustments to headerHeight when UIDevice has top notch
+//    static func getHeaderHeight() -> CGFloat {
+//        if UIDevice.current.hasNotch {
+//            return 104.98
+//        } else {
+//            return 80.98
+//        }
+//    }
+//}
