4651cb5129cee2555301f7774ffcdfbf1280e8f1
Michael Raffin <michaelraffinpaculba@gmail.com>
2019-12-27 12:48:46 +0800

    Adding  Utilities for views

---
+++ CEBComponentsLibrary/Utilities/ViewSize.swift
@@ -0,0 +1,21 @@
+//
+//  ViewSize.swift
+//  CEBComponentsLibrary
+//
+//  Created by Nicole Jillian Day on 27/12/2019.
+//
+
+import Foundation
+
+
+class ViewSizeUtil {
+    
+    //should make adjustments to headerHeight when UIDevice has top notch
+    static func getHeaderHeight() -> CGFloat {
+        if UIDevice.current.hasNotch {
+            return 104.98
+        } else {
+            return 80.98
+        }
+    }
+}
