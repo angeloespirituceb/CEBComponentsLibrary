1244221a1f116061183d16a16d1a79df00b7856c
Michael Raffin <michaelraffinpaculba@gmail.com>
2020-01-02 09:35:26 +0800

    delete tags 2.0.5

---
+++ CEBComponentsLibrary/Example/Classes/CPMichael.swift
@@ -0,0 +1,16 @@
+//
+//  CPMichael.swift
+//  CEBComponentsLibrary
+//
+//  Created by Nicole Jillian Day on 02/01/2020.
+//
+
+import Foundation
+import UIKit
+open class CPMichael: UIView {
+
+    
+    public func getData(){
+        print("sample-->>" ,Date())
+    }
+}
